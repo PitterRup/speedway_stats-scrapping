f27efcf9d01d212595d6c28d61c4ef49cfc24915
PitterRup <pitter.rup@gmail.com>
2020-12-02 22:16:37 +0100

    Refaktoring

--- speedway_data_parser/interpreters.py
+++ speedway_data_parser/interpreters.py
@@ -1,4 +1,4 @@
-from speedway_data_parser.types import Heat, InterpretedHeatRider
+from speedway_data_parser.types import InterpretedHeat, InterpretedHeatRider
 
 
 class ParsedTeamMatchInterpreter:
@@ -7,37 +7,48 @@ class ParsedTeamMatchInterpreter:
         :param `TeamMatchParser` parsed_data: obiekt sparsowanych danych
         u'''
         self.parsed_data = parsed_data
+        self.dct_first_team_composition = {
+            rider.name: rider.number
+            for rider in self.parsed_data.get_first_team_composition()
+        }
+        self.dct_second_team_composition = {
+            rider.name: rider.number
+            for rider in self.parsed_data.get_second_team_composition()
+        }
 
     def __getattr__(self, item):
         if item == 'get_heats':
             heats = self.parsed_data.get_heats()
-            self.dct_first_team_composition = {
-                rider.name: rider.number
-                for rider in self.parsed_data.get_first_team_composition()
-            }
-            self.dct_second_team_composition = {
-                rider.name: rider.number
-                for rider in self.parsed_data.get_second_team_composition()
-            }
             ret = []
             for heat in heats:
                 dct_heat = heat._asdict()
-                dct_rider_a = dct_heat['rider_a']._asdict()
-                dct_rider_b = dct_heat['rider_b']._asdict()
-                dct_rider_c = dct_heat['rider_c']._asdict()
-                dct_rider_d = dct_heat['rider_d']._asdict()
-                dct_rider_a['number'], dct_rider_a['team'] = self.identify_rider(dct_rider_a['name'])
-                dct_rider_b['number'], dct_rider_b['team'] = self.identify_rider(dct_rider_b['name'])
-                dct_rider_c['number'], dct_rider_c['team'] = self.identify_rider(dct_rider_c['name'])
-                dct_rider_d['number'], dct_rider_d['team'] = self.identify_rider(dct_rider_d['name'])
-                dct_heat['rider_a'] = InterpretedHeatRider._make(dct_rider_a.values())
-                dct_heat['rider_b'] = InterpretedHeatRider._make(dct_rider_b.values())
-                dct_heat['rider_c'] = InterpretedHeatRider._make(dct_rider_c.values())
-                dct_heat['rider_d'] = InterpretedHeatRider._make(dct_rider_d.values())
-                ret.append(Heat._make(dct_heat.values()))
+                dct_heat['finished'] = heat.winner_time is not None
+                (
+                    dct_heat['rider_a'],
+                    dct_heat['rider_b'],
+                    dct_heat['rider_c'],
+                    dct_heat['rider_d'],
+                ) = self.identify_riders(heat)
+                ret.append(InterpretedHeat._make(dct_heat.values()))
             return ret
         else:
             return getattr(self.parsed_data, item)
+
+    def identify_riders(self, heat):
+        dct_rider_a = heat.rider_a._asdict()
+        dct_rider_b = heat.rider_b._asdict()
+        dct_rider_c = heat.rider_c._asdict()
+        dct_rider_d = heat.rider_d._asdict()
+        dct_rider_a['number'], dct_rider_a['team'] = self.identify_rider(dct_rider_a['name'])
+        dct_rider_b['number'], dct_rider_b['team'] = self.identify_rider(dct_rider_b['name'])
+        dct_rider_c['number'], dct_rider_c['team'] = self.identify_rider(dct_rider_c['name'])
+        dct_rider_d['number'], dct_rider_d['team'] = self.identify_rider(dct_rider_d['name'])
+        return (
+            InterpretedHeatRider._make(dct_rider_a.values()),
+            InterpretedHeatRider._make(dct_rider_b.values()),
+            InterpretedHeatRider._make(dct_rider_c.values()),
+            InterpretedHeatRider._make(dct_rider_d.values()),
+        )
 
     def identify_rider(self, rider_name):
         number = self.dct_first_team_composition.get(rider_name)
--- speedway_data_parser/types.py
+++ speedway_data_parser/types.py
@@ -3,7 +3,18 @@ from collections import namedtuple
 TeamCompositionRider = namedtuple("TeamCompositionRider", ["number", "name"])
 
 
-Heat = namedtuple("Heat", ["number", "winner_time", "rider_a", "rider_b", "rider_c", "rider_d"])
+Heat = namedtuple(
+    "Heat", [
+        "number", "winner_time", "rider_a", "rider_b", "rider_c", "rider_d"
+    ]
+)
+
+InterpretedHeat = namedtuple(
+    "InterpretedHeat",
+    list(Heat._fields) + [
+        'finished',
+    ]
+)
 
 
 HeatRider = namedtuple(
